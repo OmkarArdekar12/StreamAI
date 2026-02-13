d955a70911dd0a3785d00a1753f42799a5625894
OmkarArdekar12 <omkarardekar12@gmail.com>
2026-02-13 22:50:57 +0530

    Navbar

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -24,3 +24,30 @@ export default function ProtectedRoute({ children }) {
 
   return children;
 }
+
+// import React, { useEffect } from "react";
+// import { Navigate } from "react-router-dom";
+// import { useAuthStore } from "../store/useAuthStore";
+// import { Loader } from "lucide-react";
+
+// export default function ProtectedRoute({ children }) {
+//   const { authUser, isAuthenticate, isCheckingAuth } = useAuthStore();
+
+//   useEffect(() => {
+//     isAuthenticate();
+//   }, []);
+
+//   if (isCheckingAuth && !authUser) {
+//     return (
+//       <div className="bg-[#0e0e0e] flex items-center justify-center h-screen">
+//         <Loader className="size-10 animate-spin" />
+//       </div>
+//     );
+//   }
+
+//   if (!authUser) {
+//     return <Navigate to="/login" replace />;
+//   }
+
+//   return children;
+// }
